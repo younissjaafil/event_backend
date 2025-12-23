9c597b6adea8a31169dce3fd7390df4ad8cfbf6e
Younissjaafil <younissjaafil@gmail.com>
2025-12-23 13:43:35 +0200

    refactor: switch from single connection to connection pool for MySQL database

--- config/db.js
+++ config/db.js
@@ -13,6 +13,11 @@ if (process.env.DATABASE_URL) {
     user: dbUrl.username,
     password: dbUrl.password,
     database: dbUrl.pathname.slice(1), // Remove leading '/'
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
     authPlugins: {
       mysql_clear_password: () => () => Buffer.from(dbUrl.password + '\0')
     }
@@ -23,25 +28,32 @@ if (process.env.DATABASE_URL) {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
     authPlugins: {
       mysql_clear_password: () => () => Buffer.from(process.env.DB_PASSWORD + '\0')
     }
   };
 }
 
-const db = mysql.createConnection(dbConfig);
+// Create connection pool instead of single connection
+const pool = mysql.createPool(dbConfig);
 
-// Connect to database
-db.connect((err) => {
+// Test the connection
+pool.getConnection((err, connection) => {
   if (err) {
     console.error("Error connecting to MySQL database:", err);
     return;
   }
   console.log("Connected to MySQL database:", dbConfig.host);
+  connection.release();
 });
 
 // Use promises for async/await support
-const dbPromise = db.promise();
+const dbPromise = pool.promise();
 
-module.exports = { db, dbPromise };
+module.exports = { db: pool, dbPromise };
 
